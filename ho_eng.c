355c4ad2b88ff250a773cf30d65c5a8a6a7a46bc
João Paulo Melo de Sampaio <jpmelos@jpmelos.com>
2011-08-05 15:33:17 -0300

    ho_eng.c: Kernel coding style adoption.
    Signed-off-by: João Paulo Melo de Sampaio <jpmelos@jpmelos.com>

--- ho_eng.c
+++ ho_eng.c
@@ -1,36 +1,32 @@
 /*
-	Handover engine: monitors thresholds and decides upon handover
-*/
+ * Handover engine: monitors thresholds and decides upon handover.
+ * Waits for notifications arriving from the local LINK layer or updates about
+ * subscribed events from remote MIHFs.
+ */
 
-/*
-	Waits for notifications arriving from the local LINK layer
-or updates about subscribed events from remote MIHFs
-*/
-
-int
-HOEngine(void *data)
+int HOEngine(void *data)
 {
 	printk(KERN_INFO "Handover Engine thread active\n");
 
-	// register with local L2 entity
-	// register with remote MIHF / MIHIS
-	// subscribes to local events
-	// subscribes to remote events
+	/*
+	 * Register with local L2 entity.
+	 * Register with remote MIHF / MIHIS.
+	 * Subscribes to local events.
+	 * Subscribes to remote events.
+	 */
 
-	while(1) {
-		// waits on events notifications: indications
+	while (1) {
+		/* Waits on events notifications: indications. */
 
 		msleep(5000);
-		// printk(KERN_INFO "HOEngine");
 
-		// decides on starting a handover
+		/* Decides on starting a handover. */
 
 		if (kthread_should_stop())
 			break;
 	}
+
 	printk(KERN_INFO "Handover Engine thread stopping\n");
 
 	return 0;
 }
-
-
