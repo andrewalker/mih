aefb7e9170961f2994df01142344fa5d62da1782
João Paulo Melo de Sampaio <jpmelos@jpmelos.com>
2011-08-12 15:33:47 -0300

    message.h: Kernel coding style adoption.
    Signed-off-by: João Paulo Melo de Sampaio <jpmelos@jpmelos.com>

--- message.h
+++ message.h
@@ -1,19 +1,12 @@
-
 int TidInit(void);
-
 int NewTid(void);
 
-// utility functions for message handling
-
-mih_tlv_t * NewTlv(unsigned char type, unsigned char * value, int length);
-
+/* Utility functions for message handling. */
+mih_tlv_t *NewTlv(unsigned char type, unsigned char *value, int length);
 int TlvPack(unsigned char *buf, mih_tlv_t *tlv);
-
 int TlvUnpack(unsigned char *buf, mih_tlv_t *tlv);
 
-
-mih_message_t * MUnpack(unsigned char *msgseq, int seqlen);
-
-unsigned char * MPack(mih_message_t *message, int *packet_len);
+mih_message_t *MUnpack(unsigned char *msgseq, int seqlen);
+unsigned char *MPack(mih_message_t *message, int *packet_len);
 
 
